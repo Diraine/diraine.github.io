870316d07bb66ae3714ee013927e0e6126e8003b
diraine <diraine29@gmail.com>
2024-09-26 17:26:33 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Simple Form Submission Script
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message Sent! Thank you for contacting me.');
+});
